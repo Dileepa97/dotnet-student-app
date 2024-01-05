74470e3f99b6433126e9ddd4426d64f70b1ceda6
Dileepa97 <54783169+Dileepa97@users.noreply.github.com>
2024-01-05 11:18:14 +0530

    Teacher Model Updated

--- student-mgt-app/Models/Domain/Teacher.cs
+++ student-mgt-app/Models/Domain/Teacher.cs
@@ -3,32 +3,24 @@ using System.Runtime.Serialization;
 
 namespace student_mgt_app.Models.Domain
 {
-    [DataContract]
+
     public class Teacher
     {
 
-        [DataMember(Name = "Id")]
         public Guid Id { get; set; }
 
-        [DataMember(Name = "FirstName")]
         public string FirstName { get; set; }
 
-        [DataMember(Name = "LastName")]
         public string LastName { get; set; }
 
-        [DataMember(Name = "ContactNo")]
         public string ContactNo { get; set; }
 
-        [DataMember(Name = "Email")]
         public string Email { get; set; }
 
-        [DataMember(Name = "CreatedDate")]
         public DateTime CreatedDate { get; set; }
 
-        [DataMember(Name = "LastUpdatedDate")]
         public DateTime LastUpdatedDate { get; set; }
 
-        [DataMember(Name = "IsActive")]
         public bool IsActive { get; set; }
     }
 }
